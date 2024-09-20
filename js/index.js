958c196aa1558f542f89c4533aa6cccdf78eb57e
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-20 19:00:59 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const item2 = document.getElementById("item2");
+const parent = item2.parentElement;
+console.log(parent);
+
+const previousSibling = item2.previousElementSibling;
+console.log(previousSibling);
+
+const nextSibling = item2.nextElementSibling;
+console.log(nextSibling);
