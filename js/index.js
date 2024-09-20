9d9d92d526730eff1f4955df4101cb63116d42e6
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-09-20 19:43:26 +0600

    done

--- js/index.js
+++ js/index.js
@@ -5,5 +5,13 @@ console.log(parent);
 const previousSibling = item2.previousElementSibling;
 console.log(previousSibling);
 
-const nextSibling = item2.nextElementSibling;
+const nextSibling = item2.nextSibling;
 console.log(nextSibling);
+
+const nextElementSibling = item2.nextElementSibling;
+console.log(nextElementSibling);
+
+const container4 = document.querySelector(".container4");
+console.log(container4.innerText);
+console.log(container4.textContent);
+console.log(container4.innerHTML);
